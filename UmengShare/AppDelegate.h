cba5a14b53e3f31d10f4c6992ccb2e06a1a5b992
Yjson <532966908@qq.com>
2015-03-24 15:00:39 +0800

    UMeng Share Demo
    This is a Demo about UMeng Share

---
+++ UmengShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UmengShare
+//
+//  Created by KM on 15-3-24.
+//  Copyright (c) 2015年 YaShen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UmengShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UmengShare
+//
+//  Created by KM on 15-3-24.
+//  Copyright (c) 2015年 YaShen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
